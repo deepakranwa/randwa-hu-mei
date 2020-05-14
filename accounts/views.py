612abdacfa7fafa947f16d0135069f7a25b72b0e
Yash Verma <yashv87@gmail.com>
2020-05-14 14:46:07 +0530

    Login,Logout,Signup and all work done
     Now sign up signs up and logs in also.. and log in page actually works.. added More comments.used login()
    
     made a f7unctional logout button.. and added important comment in logout line of urls.py
    
     pata nhi bhai restore kia h

--- accounts/views.py
+++ accounts/views.py
@@ -2,16 +2,42 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render,redirect
-from django.contrib.auth import forms
+from django.http import HttpResponse
+from django.contrib.auth import forms, login ,logout  # .auth means authentication models.. mah nig
 
 # Create your views here.
 def signup_view(request):
     if (request.method =='POST'):
-        form=forms.UserCreationForm(request.POST) #isse jo dataa aaya post request k saath usse form bnega and saved in form
+        form=forms.UserCreationForm(data=request.POST) #isse jo dataa aaya post request k saath usse form bnega and saved in form
         if (form.is_valid()):
-            form.save()
-            return redirect('articles:list')
+            user=form.save()  # this does 2 things... savesthe form(sign ups the user) and also returns the user(name) ... 
+            login(request,user)
+            return redirect('articles:list')# so now it signs up and also logs in ... and then rediredct to articles page 
         
     else:  # request is get.. so what u do is .. take the required form and send it in the template 
         form=forms.UserCreationForm()
     return render(request,'signup_page.html',{'form_info':form})  # by default request is get.. 
+
+def login_page(request):
+    if(request.method=='POST'): 
+        print("ok")
+        #agr user hai toh log in else.. dekh le 
+        form=forms.AuthenticationForm(data=request.POST) #django forms not only give Forms ka code..  but give the CORRESPONDING validation way also!
+        # by validation i dont mean that password>8 char & all... it also means.. wheather user exists in database or not! Thats the magic 
+        if(form.is_valid()):
+            user=form.get_user()
+            login(request,user)# log the user in 
+            # if next naamak info aaya hai request k 7.... toh uski value pe redirect 
+            if( 'next' in request.POST):
+                return redirect(request.POST.get('next'))
+            return redirect('articles:list')
+
+
+    else:
+        form=forms.AuthenticationForm()
+    return render(request,'login_template.html',{'form':form})
+
+def logout_page(request):
+    if(request.method=='POST'):
+        logout(request)
+        return redirect('articles:list')
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Article
-
+from django.contrib.auth.decorators import login_required    # its  a decorator.. see other decoratos on google
+# just add above definition of functions whose view req. login.. argument is .. where to redirect if ! logged in 
 def article_list(request):
     articles = Article.objects.all().order_by('date');
     return render(request, 'articles/article_list.html', { 'articles': articles })
@@ -9,3 +10,8 @@ def article_list(request):
 def article_detail(request, slug):
     deets=Article.objects.get(slug=slug)
     return render(request, 'articles/article_detailed.html',{'slug_art':deets})
+
+
+@login_required(login_url="/accounts/login/")
+def create_article(request):
+    return render(request,'create_article.html')
